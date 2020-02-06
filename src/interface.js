6938ef21669cf5269fd88f79eb443f4d8076bd53
ansaryergesh <yergeshovansar@gmail.com>
2020-02-06 22:00:23 +0600

    position with x and y

--- src/interface.js
+++ src/interface.js
@@ -4,7 +4,11 @@ const display = (() => {
     const computer = document.querySelector('.computer');
     let board = '';
     for (let i = 0; i < 100; i += 1) {
-        board += `<div data-position=${i} class="box"></div>`;
+        for (let x = 0; x < 10; x += 1) {
+            for (let y = 0; y < 10; y += 1) {
+                board += `<div data-position-x=${x} data-position-y=${y} class="box"></div>`;
+            }
+        }
     }
     human.innerHTML = board;
     computer.innerHTML = board;
