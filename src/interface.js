63d109e8ae2f93ddefd4b479b5e6063ac8d99b5b
ansaryergesh <yergeshovansar@gmail.com>
2020-02-06 23:37:52 +0600

    turn message interface

--- src/interface.js
+++ src/interface.js
@@ -1,7 +1,9 @@
 const display = (() => {
   const render = () => {  
+    const board = document.querySelector('.board');
     const human = document.querySelector('.human');
     const computer = document.querySelector('.computer');
+    const message = document.querySelector('.messageText');
     let board = '';
     for (let x = 0; x < 10; x += 1) {
         for (let y = 0; y < 10; y += 1) {
@@ -11,6 +13,8 @@ const display = (() => {
     
     human.innerHTML = board;
     computer.innerHTML = board;
+    let turn = 'Player turn'
+    message.innerText = 'Player turn'
   }
 
   return { render}
