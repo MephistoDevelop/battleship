78474c4d404551bb380113af2529470a305f7799
ansaryergesh <yergeshovansar@gmail.com>
2020-02-13 01:54:54 +0600

    hit test, with error

--- test/ship.test.js
+++ test/ship.test.js
@@ -2,14 +2,13 @@ import ship from '../src/ships';
 import gameBoard from '../src/GameBoard';
 
 const shipElem = ship(2, 'Destroyer');
-const board = gameBoard.drawBoardPlayer;
-// const position = parseInt(board[2]).split('');
 test('length of ship',() => {
   expect(shipElem.Lengths).toBe(2);
 });
 
 test('hit the ship',() => {
-  // shipElem.hit(shipElem.ships,  0, shipElem.Name);
-  // expect(shipElem.isSunked).toBe(false);
-  gameBoard.receiveAtack(1, 1, board, shipElem.ships)
+  const name = shipElem.Name;
+  const ships = shipElem.ships;
+  shipElem.hit(ships,0,name);
+  expect(ships[name]).toBe(['X']);
 });
