7fd16d98a94d69a9f864c4ac1e4a9f7b7ed85fe9
ansaryergesh <yergeshovansar@gmail.com>
2020-02-16 18:37:28 +0600

    button style

--- test/player.test.js
+++ test/player.test.js
@@ -29,5 +29,5 @@ test('computer move',() => {
   // board.placeShip(shipElem,br,1,1);
   const move =pl.Move(1, x, y, board, ships);
   let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  expect(numbers.includes(move[1])).toBe(true);
+  expect(numbers.includes(move[1]) && numbers.includes(move[0])).toBe(true);
 });
