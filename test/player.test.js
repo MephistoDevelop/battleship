2fe220a8f81152c03c5baa3ebcb09a72c1cecd36
ansaryergesh <yergeshovansar@gmail.com>
2020-02-19 00:14:27 +0600

    delete unuseful comments

--- test/player.test.js
+++ test/player.test.js
@@ -26,7 +26,6 @@ test('computer move',() => {
   const br = board.drawBoardPlayer();
   board.Board = br;
   board.BoardComputer = br;
-  // board.placeShip(shipElem,br,1,1);
   const move =pl.Move(1, x, y, board, ships);
   let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   expect(numbers.includes(move[1]) && numbers.includes(move[0])).toBe(true);
