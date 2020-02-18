9817e811fabc804587849eca91f0b5a5d5f8b1b3
MephistoDevelop <inesherald@gmail.com>
2020-02-18 11:14:58 -0600

    Fix gameboard tests

--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -2,63 +2,64 @@ import GameBoard from '../src/GameBoard';
 import ship from '../src/ships';
 import player from '../src/players';
 
-test ('draw board',() => {
+test('draw board', () => {
   const board = GameBoard();
   const br = board.drawBoardPlayer();
   expect(br[1].length).toBe(10);
 });
 
-test('placeShip horizontally',() => {
+test('placeShip horizontally', () => {
   const board = GameBoard();
   const br = board.drawBoardPlayer();
   const shipElem = ship(2, 'Destroyer');
-  const place = board.placeShip(shipElem,br,1,1);
+  const place = board.placeShip(shipElem, br, 1, 1);
   const newboard = br;
-  expect(newboard[1][2]).toBe("Destroyer1");
+  expect(newboard[1][2]).toBe('Destroyer1');
 });
 
-test('placeShip vertically',() => {
+test('placeShip vertically', () => {
   const board = GameBoard();
   const br = board.drawBoardPlayer();
   const pl = player('Ansar');
   const shipElem = ship(2, 'Destroyer');
-  const place = board.placeShip(shipElem,br,1,1, true);
+  const place = board.placeShip(shipElem, br, 1, 1, true);
   const newboard = br;
-  expect(newboard[2][1]).toBe("Destroyer1");
+  expect(newboard[2][1]).toBe('Destroyer1');
 });
 
-test('receiveAttack for empty cell',() => {
+test('receiveAttack for empty cell', () => {
   const board = GameBoard();
   const br = board.drawBoardPlayer();
   const shipElem = ship(2, 'Destroyer');
   let ships = [];
   ships.push(shipElem);
-  const attackEmpty = board.receiveAtack(1,1,br,ships);
+  const attackEmpty = board.receiveAtack(1, 1, br, ships);
   expect(attackEmpty).toBe(false);
 });
 
-// test('receiveAttack for cell with ship',() => {
-//   const board = GameBoard();
-//   const br = board.drawBoardPlayer();
-//   const shipElem = ship(2, 'Destroyer');
-//   board.placeShip(shipElem,br,1, 1, true);
-//   let ships = [];
-//   shipElem.fill_ship(shipElem.Lengths);
-//   ships.push(shipElem);
-//   const newboard = br;
-//   const attack = board.receiveAtack(1, 1, newboard,ships);
-//   expect(attack).toBe(true);
-// });
+test('receiveAttack for cell with ship', () => {
+  const shipElem = ship(2, 'Destroyer');
+  const shipArray = shipElem.ships;
+  const arr = shipElem.fill_ship(shipElem.Lengths);
+  shipElem.ships[shipElem.Name] = arr;
+  const board = GameBoard();
+  const br = board.drawBoardPlayer();
+  board.placeShip(shipElem, br, 1, 1, true);
+  const hit = board.receiveAtack(1, 1, br, [shipElem, 1]);
+  expect(hit).toBe(true);
+});
 
-// test('check if sunked',() => {
-//     const board = GameBoard();
-//     const br = board.drawBoardPlayer();
-//     const shipElem = ship(2, 'Destroyer');
-//     board.placeShip(shipElem,br,1,1);
-//     let ships = [];
-//     ships.push(shipElem);
-//     const newboard = br;
-//     board.receiveAtack(1, 1,newboard,ships);
-//     board.receiveAtack(1, 2,newboard,ships);
-//     expect(ship.isSunk(shipElem)).toBe(true);
-// })
+test('check if sunked', () => {
+  const shipElem = ship(3, 'Carrierr');
+  const shipArray = shipElem.ships;
+  const arr = shipElem.fill_ship(shipElem.Lengths);
+  shipElem.ships[shipElem.Name] = arr;
+  const board = GameBoard();
+  const br = board.drawBoardPlayer();
+  board.placeShip(shipElem, br, 1, 1, true);
+  board.receiveAtack(1, 1, br, [shipElem, 1]);
+  board.receiveAtack(1, 2, br, [shipElem, 1]);
+  board.receiveAtack(1, 3, br, [shipElem, 1]);
+  console.log('Soy Arr of sunked test:' + JSON.stringify(shipArray));
+  expect(shipElem.isSunk(shipArray[shipElem.Name])).toBe(true);
+});
